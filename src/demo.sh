4e5f9dbe9b87a814a093aa950283cff35068130f
Qtieee <tungdop2@gmail.com>
2022-03-17 08:44:43 +0700

    Update demo.sh

--- src/demo.sh
+++ src/demo.sh
@@ -1,9 +1,12 @@
 if [ -f "weights/crowdhuman_yolov5m.pt" ]; then
     echo "weight prepared."
-else 
-    echo "weight prepared."
+else
+    mkdir -p weights
+    cd weights
+    echo "download weight."
     gdown --id 1gglIwqxaH2iTvy6lZlXuAcMpd_U0GCUb
 fi
 
+cd ..
 read var var1
 python3 src/demo.py video --device gpu --path $var --limit $var1
