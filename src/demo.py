dd7715ef34e09c602ae3ef57bbf4462423559548
Qtieee <tungdop2@gmail.com>
2022-04-02 00:07:46 +0700

    update

--- src/demo.py
+++ src/demo.py
@@ -17,7 +17,7 @@ import time
 from models.common import DetectMultiBackend
 from utils.augmentations import letterbox
 
-from facemask.resnet import resnet9, face_mask_transform
+from facemask.resnet import resnet9x128
 from distance.distance import load_top_view_config
 
 
@@ -174,12 +174,9 @@ def imageflow_demo(predictor, vis_folder, current_time, args, test_size):
     )
     person_tracker = BYTETracker(args, frame_rate=30)
     face_tracker = BYTETracker(args, frame_rate=30)
-    # face_model = face_mask_model()
-    face_model = resnet9()
-    # face_model.to('cuda' if args.device == 'gpu' else 'cpu')
-    face_model.cuda()
+    face_model, transform = resnet9x128()
+    face_model.to('cuda' if args.device == 'gpu' else 'cpu')
     face_model.eval()
-    transform = resnet9.model_transform()
     M, w_scale, h_scale = load_top_view_config('distance/distance.txt')
     timer = Timer()
     frame_id = 0
@@ -224,10 +221,10 @@ def imageflow_demo(predictor, vis_folder, current_time, args, test_size):
                 for i, t in enumerate(online_faces):
                     tlwh = t.tlwh
                     if tlwh[2] * tlwh[3] > args.min_box_area and tlwh[0] > 0 and tlwh[1] > 0 and tlwh[0] + tlwh[2] < img_info['width'] and tlwh[1] + tlwh[3] < img_info['height']:
-                        tlwh[0] = max(0, tlwh[0] - int(64 / tlwh[2]))
-                        tlwh[1] = max(0, tlwh[1] - int(64 / tlwh[3]))
-                        tlwh[2] = min(img_info['width'] - tlwh[0], tlwh[2] + int(128 / tlwh[2]))
-                        tlwh[3] = min(img_info['height'] - tlwh[1], tlwh[3] + int(128 / tlwh[3]))
+                        tlwh[0] = max(0, tlwh[0] - int(128 / tlwh[2]))
+                        tlwh[1] = max(0, tlwh[1] - int(128 / tlwh[3]))
+                        tlwh[2] = min(img_info['width'] - tlwh[0], tlwh[2] + int(256 / tlwh[2]))
+                        tlwh[3] = min(img_info['height'] - tlwh[1], tlwh[3] + int(256 / tlwh[3]))
                         face = frame[int(tlwh[1]):int(tlwh[1] + tlwh[3]), int(tlwh[0]):int(tlwh[0] + tlwh[2])]
                         # cv2.imwrite(f'{frame_id}_{i}.jpg', face)
                         face = transform(face)
--- src/facemask/resnet.py
+++ src/facemask/resnet.py
@@ -4,47 +4,49 @@ import torchvision
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 
+
 def conv_block(in_channels, out_channels, pooling=False):
-        '''
-        params: in_channels: (int) number of input channels
-        params: out_channels: (int) number of output channels
-        params: pooling: (bool) use pooling or not
-        return: convolutional layers
-        '''
-        conv_layers = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU()
-            )
-        if pooling: 
-            conv_layers.add_module('max_pooling',nn.MaxPool2d(2))
-        return conv_layers
+    '''
+    params: in_channels: (int) number of input channels
+    params: out_channels: (int) number of output channels
+    params: pooling: (bool) use pooling or not
+    return: convolutional layers
+    '''
+    conv_layers = nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU()
+    )
+    if pooling:
+        conv_layers.add_module('max_pooling', nn.MaxPool2d(2))
+    return conv_layers
+
 
 class ResNet9x128(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
 
-        #1st Block
-        self.conv1 = conv_block(in_channels, 64)#input size 1*128*128
-        self.conv2 = conv_block(64, 128, True) #After pooling 64*64*64
-        #Residual layer
-        self.res1 = nn.Sequential(conv_block(128,128), conv_block(128,128))
-        
-        #2nd Block
-        self.conv3 = conv_block(128, 256, True) #After pooling 256*32*32
-        self.conv4 = conv_block(256, 512, True) #After pooling 512*16*16
-        #Residual layer
-        self.res2 = nn.Sequential(conv_block(512,512), conv_block(512,512))
-        
-        #Linear Network
-        self.linear = nn.Sequential(
-            nn.MaxPool2d(16), #After pooling 512*1*1
-            nn.Flatten(), # 512
-            nn.Linear(512, num_classes),
-            )
+        # 1st Block
+        self.conv1 = conv_block(in_channels, 64)  # input size 1*128*128
+        self.conv2 = conv_block(64, 128, True)  # After pooling 64*64*64
+        # Residual layer
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
 
-    def forward(self,x):
-        #Block-1
+        # 2nd Block
+        self.conv3 = conv_block(128, 256, True)  # After pooling 256*32*32
+        self.conv4 = conv_block(256, 512, True)  # After pooling 512*16*16
+        # Residual layer
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+
+        # Linear Network
+        self.linear = nn.Sequential(
+            nn.MaxPool2d(16),  # After pooling 512*1*1
+            nn.Flatten(),  # 512
+            nn.Linear(512, num_classes),
+        )
+
+    def forward(self, x):
+        # Block-1
         out = self.conv1(x)
         out = self.conv2(out)
         res1 = self.res1(out) + out
@@ -57,48 +59,33 @@ class ResNet9x128(nn.Module):
         # #Linear network
         out = self.linear(res2)
         return out
-    
-    def model_transform():
-        return transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize((128, 128)),
-            transforms.Grayscale(1),
-            transforms.Normalize(0.5, 0.5)
-        ])
 
-def face_mask_transform():
-    return transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Resize((128, 128)),
-        transforms.Grayscale(1),
-        transforms.Normalize(0.5, 0.5)
-    ])
 
 class ResNet9x64(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
 
-        #1st Block
-        self.conv1 = conv_block(in_channels, 64)#input size 1*128*128
-        self.conv2 = conv_block(64, 128, True) #After pooling 64*64*64
-        #Residual layer
-        self.res1 = nn.Sequential(conv_block(128,128), conv_block(128,128))
-        
-        #2nd Block
-        self.conv3 = conv_block(128, 256, True) #After pooling 256*32*32
-        self.conv4 = conv_block(256, 512, True) #After pooling 512*16*16
-        #Residual layer
-        self.res2 = nn.Sequential(conv_block(512,512), conv_block(512,512))
-        
-        #Linear Network
-        self.linear = nn.Sequential(
-            nn.MaxPool2d(16), #After pooling 512*1*1
-            nn.Flatten(), # 512
-            nn.Linear(512, num_classes),
-            )
+        # 1st Block
+        self.conv1 = conv_block(in_channels, 64)  # input size 1*128*128
+        self.conv2 = conv_block(64, 128, True)  # After pooling 64*64*64
+        # Residual layer
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
 
-    def forward(self,x):
-        #Block-1
+        # 2nd Block
+        self.conv3 = conv_block(128, 256, True)  # After pooling 256*32*32
+        self.conv4 = conv_block(256, 512, True)  # After pooling 512*16*16
+        # Residual layer
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+
+        # Linear Network
+        self.linear = nn.Sequential(
+            nn.MaxPool2d(16),  # After pooling 512*1*1
+            nn.Flatten(),  # 512
+            nn.Linear(512, num_classes),
+        )
+
+    def forward(self, x):
+        # Block-1
         out = self.conv1(8)
         out = self.conv2(out)
         res1 = self.res1(out) + out
@@ -112,8 +99,13 @@ class ResNet9x64(nn.Module):
         out = self.linear(res2)
         return out
 
-def resnet9():
+def resnet9x128():
     model = ResNet9x128(1, 2)
     ckpt = 'facemask/x128.pt'
     model.load_state_dict(torch.load(ckpt, map_location='cpu'))
-    return model
+    return model, transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((128, 128)),
+        transforms.Grayscale(1),
+        transforms.Normalize(0.5, 0.5)
+    ])
