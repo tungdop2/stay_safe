551c208b4b330a816d4d02695aa224c829b63ed3
Qtieee <tungdop2@gmail.com>
2022-04-03 22:32:53 +0700

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -221,10 +221,10 @@ def imageflow_demo(predictor, vis_folder, current_time, args, test_size):
                 for i, t in enumerate(online_faces):
                     tlwh = t.tlwh
                     if tlwh[2] * tlwh[3] > args.min_box_area and tlwh[0] > 0 and tlwh[1] > 0 and tlwh[0] + tlwh[2] < img_info['width'] and tlwh[1] + tlwh[3] < img_info['height']:
-                        tlwh[0] = max(0, tlwh[0] - int(128 / tlwh[2]))
-                        tlwh[1] = max(0, tlwh[1] - int(128 / tlwh[3]))
-                        tlwh[2] = min(img_info['width'] - tlwh[0], tlwh[2] + int(256 / tlwh[2]))
-                        tlwh[3] = min(img_info['height'] - tlwh[1], tlwh[3] + int(256 / tlwh[3]))
+                        tlwh[0] = max(0, tlwh[0] - int(256 / tlwh[2]))
+                        tlwh[1] = max(0, tlwh[1] - int(256 / tlwh[3]))
+                        tlwh[2] = min(img_info['width'] - tlwh[0], tlwh[2] + int(512 / tlwh[2]))
+                        tlwh[3] = min(img_info['height'] - tlwh[1], tlwh[3] + int(512 / tlwh[3]))
                         face = frame[int(tlwh[1]):int(tlwh[1] + tlwh[3]), int(tlwh[0]):int(tlwh[0] + tlwh[2])]
                         # cv2.imwrite(f'{frame_id}_{i}.jpg', face)
                         face = transform(face)
@@ -268,9 +268,6 @@ def main(args):
         model.model.half()
     model.eval()
 
-    # if args.ckpt.find('yolov5s') != -1:
-    #     test_size = (416, 416)
-    # else:
     test_size = (608, 1088)
     predictor = Predictor(model, 2, conf_thresh, nms_thresh, test_size, args.device, args.fp16)
     current_time = time.localtime()
